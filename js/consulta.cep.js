525157f36ee33b8b99afe792f5624893f4b3d8a5
betossauro <pfvrbeto@gmail.com>
2023-09-08 16:47:48 -0300

    organização de arquivos

---
+++ js/consulta.cep.js
@@ -0,0 +1,60 @@
+// async function buscarCEP(cep) {
+//   limpar();
+
+//   var cep = document.getElementById("cep");
+//   var cepInformado = cep.value;
+//   cepInformado = cepInformado.replace('-', '');
+
+//   if (cepInformado.length < 8 || cepInformado.length > 9 ) {
+//     alert('O CEP deve ser no padrão 00000000 ou 00000-000')
+//   }
+
+//   fetch(`https://viacep.com.br/ws/${cepInformado}/json/`)
+//     .then((resultado) => resultado.json())
+//     .then((json) => {
+//       if (json.erro) {
+//         mostrarTelaErro();
+//       } else {
+//         preencherCamposComJSON(json);
+//       }
+//     })
+//     .catch((erro) => mostrarTelaErro());
+// }
+
+// function preencherCamposComJSON(json) {
+//   if (json.bairro) {
+//     bairro.value = json.bairro;
+//   } else {
+//     bairro.disabled = false;
+//   }
+
+//   if (json.logradouro) {
+//     logradouro.value = json.logradouro;
+//   } else {
+//     logradouro.disabled = false;
+//   }
+
+//   cidade.value = json.localidade;
+//   uf.value = json.uf;
+
+//   console.log(json);
+// }
+
+// function limpar() {
+//   cep.valeu = "";
+//   bairro.value = "";
+//   logradouro.value = "";
+//   cidade.value = "";
+//   uf.value = "";
+//   numero.value = "";
+
+//   bairro.disabled = true;
+//   logradouro.disabled = true;
+//   cidade.disabled = true;
+//   uf.disabled = true;
+// }
+
+// function mostrarTelaErro() {
+//     alert('CEP informado não existe')
+//     limpar();
+// }
--- js/exemplos.js
+++ js/exemplos.js
@@ -1,59 +1,59 @@
-// Exemplo 01
+// // Exemplo 01
 
-let prompt = require('prompt-sync')();
+// let prompt = require('prompt-sync')();
 
-// let curso = prompt('Qual o nome do seu curso? ');
-// console.log('Curso: ' + curso);
+// // let curso = prompt('Qual o nome do seu curso? ');
+// // console.log('Curso: ' + curso);
 
-// let fase = Number(prompt('Informe a fase: '))
-// console.log('Fase: ' + fase + 'ª');
+// // let fase = Number(prompt('Informe a fase: '))
+// // console.log('Fase: ' + fase + 'ª');
 
-// // Exemplo 02
+// // // Exemplo 02
 
-// let carroA = {fabricante: 'Ford', modelo: 'Mustang', ano: 1969};
-// let carroB = {fabricante: 'Porsche', modelo: 'Macan', ano: 2023};
-// let carros = [carroA, carroB];
+// // let carroA = {fabricante: 'Ford', modelo: 'Mustang', ano: 1969};
+// // let carroB = {fabricante: 'Porsche', modelo: 'Macan', ano: 2023};
+// // let carros = [carroA, carroB];
 
-// for (let carro of carros) {
-//     console.log(carro);
+// // for (let carro of carros) {
+// //     console.log(carro);
+// // }
+
+// // console.log('----------------------');
+// // for (let propriedade in carroA) {
+// //     console.log(propriedade);
+// // }
+
+// // console.log('----------------------');
+// // for (let propriedade in carroB) {
+// //     console.log('Propriedade: ' + propriedade + ' - valor: ' + carroB[propriedade]);
+// // }
+
+// // console.log('----------------------');
+// // let nomes = ['Fred', 'Sheila', 'Sara'];
+// // for (let nome of nomes) {
+// //     console.log(nome);
+// // }
+
+// // console.log('----------------------');
+// // for (let nome in nomes) {
+// //     console.log(nome);
+// // }
+
+// // Exemplo CEP
+
+// let cepInformado = prompt('Informe seu CEP: ');
+
+// buscarCEP(cepInformado);
+
+// async function buscarCEP(cep) {
+//     let options = {
+//         method: 'GET',
+//         headers: {'Content-type': 'application/json'}
+//     };
+
+//     const promiseConsultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`, options); 
+
+//     const json = await promiseConsultaCEP.json();
+
+//     console.log(json);
 // }
-
-// console.log('----------------------');
-// for (let propriedade in carroA) {
-//     console.log(propriedade);
-// }
-
-// console.log('----------------------');
-// for (let propriedade in carroB) {
-//     console.log('Propriedade: ' + propriedade + ' - valor: ' + carroB[propriedade]);
-// }
-
-// console.log('----------------------');
-// let nomes = ['Fred', 'Sheila', 'Sara'];
-// for (let nome of nomes) {
-//     console.log(nome);
-// }
-
-// console.log('----------------------');
-// for (let nome in nomes) {
-//     console.log(nome);
-// }
-
-// Exemplo CEP
-
-let cepInformado = prompt('Informe seu CEP: ');
-
-buscarCEP(cepInformado);
-
-async function buscarCEP(cep) {
-    let options = {
-        method: 'GET',
-        headers: {'Content-type': 'application/json'}
-    };
-
-    const promiseConsultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`, options); 
-
-    const json = await promiseConsultaCEP.json();
-
-    console.log(json);
-}
